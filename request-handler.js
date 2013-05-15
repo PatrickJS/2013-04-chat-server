92d698f99012c5a5ff499a26df6404a21bc9ca67
Hack Reactor Students <github@hackreactor.com>
2013-05-15 16:52:59 -0700

    add posting messages

--- request-handler.js
+++ request-handler.js
@@ -1,32 +1,28 @@
-
+var stream = require('stream');
 var url = require("url");
 /* You should implement your request handler function in this file.
  * But you need to pass the function to http.createServer() in
  * basic-server.js.  So you must figure out how to export the function
  * from this file and include it in basic-server.js. Check out the
- * node module documentation at http://nodejs.org/api/modules.html. */
+ */
 
-// var pathnameRouter = function(parseUrl){
-//   switch(parseUrl.pathname) {
-//     case '/':
-//       message = 'root';
-//       break;
-//     case '/favicon.ico':
-//       message = 'favicon';
-//       break;
-//     case '/1/classes/messages':
-//       (function(query){
-//         //do something with query
-//         //return sorted messages by time "-created at"
-//       }(parseUrl.query));
-//       break;
-//     default:
-//       console.log(parseUrl.pathname);
-//       // console.log(parseUrl.query);
-//       break;
-//   }
-// };
 
+var storage = {
+  results: [{
+      'username': 'shawndrost',
+      'text': 'trololo',
+      'roomname': '4chan',
+      'hax': 'alert("hi")',
+      'createdAt': new Date()
+    },
+    {
+      'username': 'Simba',
+      'text': 'A kumba ya, ...',
+      'roomname': 'TheSavana',
+      'hax': 'alert("hi")',
+      'createdAt': new Date()
+    }]
+};
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GETS, POST, PUT, DELETE, OPTIONS",
@@ -34,23 +30,26 @@ var defaultCorsHeaders = {
   "access-control-max-age": 10 //seconds
 };
 
-var message = JSON.stringify({results:[{
-      'username': 'shawndrost',
-      'text': 'trololo',
-      'roomname': '4chan',
-      'hax': 'alert("hi")',
-      'createdAt': new Date()
-    }]});
-
 var httpMethodRouter = function(request, response){
   if(request.method === 'GET'){
     response.writeHead(200, defaultCorsHeaders);
-    response.end(message);
+    response.end(JSON.stringify(storage));
     //deal with GET
   } else if(request.method === 'POST'){
     //deal with POST
-    response.writeHead(200, defaultCorsHeaders);
-    response.end('POST!');
+    var fullBody = '';
+    request.on('data', function(chunk) {
+      console.log(chunk);
+      fullBody += chunk.toString(); //might not need to change toString because it might screw up JSON 
+    });
+    request.on('end', function() {
+      // body...
+      var data = fullBody;
+      storage.results.push(JSON.parse(data));
+      console.log(data);
+      response.writeHead(200, defaultCorsHeaders);
+      response.end('POST!');
+    });
   } else if(request.method === 'OPTIONS'){
     response.writeHead(200, defaultCorsHeaders);
     response.end('OPTIONS!');
