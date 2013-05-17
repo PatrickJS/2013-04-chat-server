db348a33c3def78319b505b4f0dcc4dc1dde7f5f
Hack Reactor Students <github@hackreactor.com>
2013-05-17 11:25:23 -0700

    ServerSpec passes

--- request-handler.js
+++ request-handler.js
@@ -1,5 +1,6 @@
 var stream = require('stream');
 var url = require("url");
+var qs = require('querystring');
 /* You should implement your request handler function in this file.
  * But you need to pass the function to http.createServer() in
  * basic-server.js.  So you must figure out how to export the function
@@ -8,20 +9,6 @@ var url = require("url");
 
 
 var storage = {
-  results: [{
-      'username': 'shawndrost',
-      'text': 'trololo',
-      'roomname': '4chan',
-      'hax': 'alert("hi")',
-      'createdAt': new Date()
-    },
-    {
-      'username': 'Simba',
-      'text': 'A kumba ya, ...',
-      'roomname': 'TheSavana',
-      'hax': 'alert("hi")',
-      'createdAt': new Date()
-    }]
 };
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
@@ -30,37 +17,39 @@ var defaultCorsHeaders = {
   "access-control-max-age": 10 //seconds
 };
 
-var httpMethodRouter = function(request, response){
-  if(request.method === 'GET'){
-    response.writeHead(200, defaultCorsHeaders);
-    response.end(JSON.stringify(storage));
-    //deal with GET
+exports.handleRequest = function(request, response) {
+  console.log('Serving awesome requests types '+ request.method + ' for url ' + request.url);
+  var url_parts = url.parse(request.url, true);
+  var pathNameArray = (url_parts.pathname).split('/');
+  var returnCode = 404;
+  var body = '\n';
+  var roomName = pathNameArray[pathNameArray.length-1];
+  var query = url_parts.query;
+
+  if (pathNameArray[1] !== 'classes') {
+  } else if(request.method === 'GET'){
+    returnCode = 200;
+    if(!storage[roomName]) {
+      storage[roomName] = [];
+    }
+    body = JSON.stringify(storage[roomName]);
   } else if(request.method === 'POST'){
-    //deal with POST
+    returnCode = 302;
     var fullBody = '';
     request.on('data', function(chunk) {
-      console.log(chunk);
-      fullBody += chunk.toString(); //might not need to change toString because it might screw up JSON 
+      fullBody += chunk.toString(); 
     });
     request.on('end', function() {
-      // body...
       var data = fullBody;
-      storage.results.push(JSON.parse(data));
-      console.log(data);
-      response.writeHead(200, defaultCorsHeaders);
-      response.end('POST!');
+      storage[roomName].unshift(qs.parse(data));
     });
   } else if(request.method === 'OPTIONS'){
-    response.writeHead(200, defaultCorsHeaders);
-    response.end('OPTIONS!');
+    returnCode = 200;
   } else {
     console.log('ERRRRORRRR i saw: ', request.method);
   }
-};
 
+  response.writeHead(returnCode, defaultCorsHeaders);
+  response.end(body);
 
-exports.handleRequest = function(request, response) {
-  console.log('Serving awesome requests types '+ request.method + ' for url ' + request.url);
-
-  httpMethodRouter(request, response);
 };
