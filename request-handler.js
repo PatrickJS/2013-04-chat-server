4fb5155876f2495f07249348b5044cec3d0bbb27
Hack Reactor Students <github@hackreactor.com>
2013-05-17 16:11:16 -0700

    add chat client, and routing

--- request-handler.js
+++ request-handler.js
@@ -1,14 +1,16 @@
 var stream = require('stream');
 var url = require("url");
-var qs = require('querystring');
+
 /* You should implement your request handler function in this file.
  * But you need to pass the function to http.createServer() in
  * basic-server.js.  So you must figure out how to export the function
  * from this file and include it in basic-server.js. Check out the
  */
 
-
 var storage = {
+  messages: {
+    "results":[]
+  }
 };
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
@@ -19,39 +21,40 @@ var defaultCorsHeaders = {
 
 exports.handleRequest = function(request, response) {
   console.log('Serving awesome requests types '+ request.method + ' for url ' + request.url);
-  var url_parts = url.parse(request.url, true);
-  var pathNameArray = (url_parts.pathname).split('/');
-  var returnCode = 404;
-  var body = '\n';  // explisit nothing
-  var roomName = pathNameArray[pathNameArray.length-1];
-  var query = url_parts.query;
+  var url_parts = url.parse(request.url, true),
+      pathNameArray = (url_parts.pathname).split('/'),
+      roomName = pathNameArray[pathNameArray.length-1],
+      query = url_parts.query,
+      returnCode = 404,
+      body = 'hello worlds';
+  //check if room exists
+  if (request.url === '/index.html' || request.url === '/') {
+    returnCode = 200;
+  }
 
-  if (pathNameArray[1] !== 'classes') {
-  } else if(request.method === 'GET'){
-    returnCode = 200;
-    if(!storage[roomName]) {
-      storage[roomName] = [];
+  if (pathNameArray[1] === 'classes') {
+    if(!storage[roomName]) {storage[roomName] = {results: []};}
+    if(request.method === 'GET'){
+      returnCode = 200;
+      body = JSON.stringify(storage[roomName]);
+    } else if(request.method === 'POST'){
+      returnCode = 201;
+      var fullBody = '';
+      request.on('data', function(chunk) {
+        fullBody += chunk;
+      });
+      request.on('end', function() {
+        var data = JSON.parse(fullBody);  
+        storage[roomName].results.unshift(data);
+      });
+    } else if(request.method === 'OPTIONS'){
+      returnCode = 200;
+    } else {
+      console.log('ERRRRORRRR i saw: ', request.method);
     }
-    body = JSON.stringify(storage[roomName]);
-  } else if(request.method === 'POST'){
-    returnCode = 302;
-    var fullBody = '';
-    request.setEncoding('utf8');
-    request.on('data', function(chunk) {
-      fullBody += chunk;
-    });
-    request.on('end', function() {
-      var data = fullBody;
-      storage[roomName].unshift(qs.parse(data));
-    });
-  } else if(request.method === 'OPTIONS'){
-    returnCode = 200;
-  } else {
-    console.log('ERRRRORRRR i saw: ', request.method);
   }
 
   response.writeHead(returnCode, defaultCorsHeaders);
-  console.log(response);
   response.end(body);
 
 };
